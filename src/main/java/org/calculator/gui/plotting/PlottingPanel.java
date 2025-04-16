85b76bdb8c4cccf563b855327d0202a93a35ac65
JJ <jack_rocks@163.com>
2025-04-16 23:54:57 +0800

    添加函数绘图的画布

--- src/main/java/org/calculator/gui/plotting/PlottingPanel.java
+++ src/main/java/org/calculator/gui/plotting/PlottingPanel.java
@@ -1,13 +1,23 @@
 package org.calculator.gui.plotting;
+
 import javax.swing.*;
 import org.calculator.math.MathEvaluator;
+import java.awt.*;
+
 public class PlottingPanel extends JPanel {
-    private GraphPanel graphpanel;
+    private GraphPanel graphPanel;
     private InputPanel inputPanel;
-    private MathEvaluator evaluator ;
+    private MathEvaluator evaluator;
 
     public PlottingPanel() {
-        GraphPanel graphpanel = new GraphPanel(this);
-        add(graphpanel, JLayeredPane.DEFAULT_LAYER);
+        // 初始化 MathEvaluator
+        evaluator = new MathEvaluator();
+        graphPanel = new GraphPanel(this);
+
+        setLayout(new BorderLayout());
+        add(graphPanel, BorderLayout.CENTER);
+
+        // 确保图形面板刷新
+        graphPanel.repaint();
     }
 }
